15e74f88977f24982c3c4d1994d61e898c8eac12
Vaclav Fencl <v_fencl@utb.cz>
2024-12-07 14:01:36 +0100

    Admin Zone

---
+++ Areas/Admin/Controllers/AdminCalendarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.OfficeCalendar.Areas.User.Controllers
+{
+    [Area("Admin")] // Specifies that this controller belongs to the "User" area
+    public class AdminCalendarController : Controller
+    {
+        public IActionResult AdminShared()
+        {
+            return View(); // Looks for CalendarDay.cshtml in Areas/User/Views/Calendar/
+        }
+
+        public IActionResult AdminMonth()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.OfficeCalendar.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+    }
+}
